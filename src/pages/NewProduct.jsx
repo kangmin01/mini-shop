c56b201f9d1cb387e3878172dcd6a22967249e23
kangmin01 <minheek99@gmail.com>
2024-01-01 23:36:58 +0900

    Create a custom hook: products

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,8 +1,7 @@
 import { useRef, useState } from "react";
 import Button from "../components/ui/Button";
 import { uploadImage } from "../api/uploader";
-import { addNewProduct } from "../api/firebase";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import useProducts from "../hooks/useProducts";
 
 export default function NewProduct() {
   const [product, setProduct] = useState({});
@@ -11,11 +10,7 @@ export default function NewProduct() {
   const [success, setSuccess] = useState();
   const imgRef = useRef(null);
 
-  const queryClient = useQueryClient();
-  const addProduct = useMutation({
-    mutationFn: ({ product, url }) => addNewProduct(product, url),
-    onSuccess: () => queryClient.invalidateQueries(["products"]),
-  });
+  const { addProduct } = useProducts();
 
   const handleChange = (e) => {
     const { name, value, files } = e.target;
